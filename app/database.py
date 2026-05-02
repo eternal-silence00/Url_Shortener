4e19cfcc1b53415804ad4183e8f475fb9fbdc68b
eternal-silence00 <andron555666777@gmail.com>
2026-05-02 19:23:06 +0300

    feat: add async database connection

---
+++ app/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from app.config import settings
+
+DATABASE_URL = settings.database_url
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo = True
+)
+
+AsyncSessionLocal = sessionmaker(
+    engine,
+    class_ = AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise 
