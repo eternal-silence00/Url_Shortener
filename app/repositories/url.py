cae4f4a49a7d2cc2623f651426a760bed2b8ff6a
eternal-silence00 <andron555666777@gmail.com>
2026-05-02 20:13:33 +0300

    feat: add URL repository

---
+++ app/repositories/url.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.models.url import Url
+
+class UrlRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        
+    async def create_url(self, url:str, short_code:str):
+        url = Url(url=url, short_code=short_code)
+        self.session.add(url)
+        await self.session.flush()
+        await self.session.refresh(url)
+        return url
+    
+    async def get_by_short_code(self, short_code: str):
+        result = await self.session.execute(select(Url).where(Url.short_code == short_code))
+        return result.scalar_one_or_none()
+    
+    async def get_all(self, limit: int = 10, offset: int = 0):
+        result = await self.session.execute(select(Url).limit(limit).offset(offset))
+        return result.scalars().all()
