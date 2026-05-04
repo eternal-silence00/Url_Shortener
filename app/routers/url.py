dcc9ec9dbd9812fcf1c021a192c55d07adf70069
eternal-silence00 <andron555666777@gmail.com>
2026-05-04 13:45:02 +0300

    feat: add response_model on post router

--- app/routers/url.py
+++ app/routers/url.py
@@ -7,7 +7,7 @@ from app.services.url import create_short_code
 
 router = APIRouter()
 
-@router.post("/url", status_code=201)
+@router.post("/url",response_model=UrlResponse, status_code=201)
 async def create_short_url(
     data: UrlCreate,
     session: AsyncSession = Depends(get_db)
