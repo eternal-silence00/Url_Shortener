40f078b7b1ce91b042a02849d9ae30c347eb0757
eternal-silence00 <andron555666777@gmail.com>
2026-05-05 19:33:23 +0300

    feat: add alembic migrations

--- app/main.py
+++ app/main.py
@@ -2,13 +2,10 @@ from fastapi import FastAPI
 from contextlib import asynccontextmanager
 from app.routers import url
 from app.database import engine 
-from app.models.url import Url, Base
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
     await engine.dispose()
     
